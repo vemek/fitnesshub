a202085e53f409b1efe4bee7b95a00c6e1e93cf6
Mark King <mark@vemek.co>
2023-12-17 20:34:32 +0000

    ignore ESLint on StackOverflow code

--- src/models/Assessment.js
+++ src/models/Assessment.js
@@ -35,8 +35,9 @@ class Assessment {
 
 function uuidv4() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        );
+      // eslint-disable-next-line no-mixed-operators
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
 }
 
 export default Assessment;
