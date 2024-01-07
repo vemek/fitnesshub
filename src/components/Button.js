2f4936d3434bd6f930f87e278cdcd1aaa81ed563
Mark King <mark@vemek.co>
2024-01-07 00:20:48 +0000

    add some colours

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
-function Button({ text, onClick }) {
+function Button({ text, onClick, type = "primary" }) {
+  const bgCol = type === "primary" ? "bg-cyan-600" : type === "danger" ? "bg-red-500" : "bg-cyan-500";
   return (
-    <button className="px-2 py-1 rounded-lg font-semibold text-sm text-white bg-indigo-400" onClick={onClick}>{text}</button>
+    <button className={`px-2 py-1 rounded-lg font-semibold text-sm text-white ${bgCol}`} onClick={onClick}>{text}</button>
   );
 }
 
--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,10 +1,18 @@
 import Button from './Button.js'
 
+function warnDeletion(user) {
+  return () => {
+    if (!window.confirm(`Are you sure you want to remove all data for ${user.name}?`)) {
+      return;
+    }
+  }
+}
+
 function Reports({ user, onClickReport }) {
   return (
     <div>
       <div className="mb-2">Select a report to view:</div>
-      <div className="grid grid-cols-[1fr_9rem_7rem] gap-2">
+      <div className="grid grid-cols-[1fr_9rem_7rem_7rem] gap-2">
         <div>
           <a href={"#report/" + user.name} onClick={onClickReport(user)}>{user.name}</a>
         </div>
@@ -12,7 +20,10 @@ function Reports({ user, onClickReport }) {
           <Button text="Add Assessment" />
         </div>
         <div>
-          <Button text="View Report" />
+          <Button text="View Report" type="secondary" onClick={onClickReport(user)} />
+        </div>
+        <div>
+          <Button text="Delete Client" type="danger" onClick={warnDeletion(user)} />
         </div>
         <div>
           <a href={"#report/" + user.name} onClick={onClickReport(user)}>{user.name}</a>
@@ -21,7 +32,10 @@ function Reports({ user, onClickReport }) {
           <Button text="Add Assessment" />
         </div>
         <div>
-          <Button text="View Report" />
+          <Button text="View Report" type="secondary" onClick={onClickReport(user)} />
+        </div>
+        <div>
+          <Button text="Delete Client" type="danger" onClick={warnDeletion(user)} />
         </div>
         <div>
           <a href={"#report/" + user.name} onClick={onClickReport(user)}>{user.name}</a>
@@ -30,9 +44,12 @@ function Reports({ user, onClickReport }) {
           <Button text="Add Assessment" />
         </div>
         <div>
-          <Button text="View Report" />
+          <Button text="View Report" type="secondary" onClick={onClickReport(user)} />
         </div>
-        <div className="col-span-3">
+        <div>
+          <Button text="Delete Client" type="danger" onClick={warnDeletion(user)} />
+        </div>
+        <div className="col-span-4">
           <Button text="Add new client" />
         </div>
       </div>
