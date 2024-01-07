016ac020a1dda233b6e6b72de3663a11d6ed6407
Mark King <mark@vemek.co>
2024-01-07 22:20:08 +0000

    justify

--- src/components/EditAssessment.js
+++ src/components/EditAssessment.js
@@ -3,40 +3,40 @@ import Input from './Input.js';
 
 function EditAssessment({ user, assessment, onDone, onUpdateAssessment }) {
   return (
-    <div className="mb-5 grid grid-cols-[16rem_1fr] gap-2 items-center max-w-2xl">
+    <div className="mb-5 grid grid-cols-1 gap-2 items-center max-w-2xl">
       <div className="col-span-2 text-4xl">Assessment for {user.name}</div>
 
-      <div className="justify-self-end">Date</div>
+      <div>Date</div>
       <div><Input type="text" onChange={onUpdateAssessment('date')} value={assessment.date} /></div>
 
-      <div className="justify-self-end">Blood Pressure</div>
+      <div>Blood Pressure</div>
       <div className="flex gap-2 w-full items-center justify-between">
         <Input type="number" onChange={onUpdateAssessment('bloodPressureSystolic')} value={assessment.bloodPressureSystolic} />
         <div className="grow text-center">/</div>
         <Input type="number" onChange={onUpdateAssessment('bloodPressureDiastolic')} value={assessment.bloodPressureDiastolic} />
       </div>
 
-      <div className="justify-self-end">Weight</div>
+      <div>Weight</div>
       <div><Input type="number" onChange={onUpdateAssessment('weight')} value={assessment.weight} unit="kg" /></div>
 
       <div className="col-span-2 text-lg underline mt-4">Measurements</div>
 
-      <div className="justify-self-end">Neck</div>
+      <div>Neck</div>
       <div><Input type="number" onChange={onUpdateAssessment('msNeck')} value={assessment.msNeck} unit="cm" /></div>
 
-      <div className="justify-self-end">Upper arm</div>
+      <div>Upper arm</div>
       <div><Input type="number" onChange={onUpdateAssessment('msUpperArm')} value={assessment.msUpperArm} unit="cm" /></div>
 
-      <div className="justify-self-end">Chest</div>
+      <div>Chest</div>
       <div><Input type="number" onChange={onUpdateAssessment('msChest')} value={assessment.msChest} unit="cm" /></div>
 
-      <div className="justify-self-end">Upper abs</div>
+      <div>Upper abs</div>
       <div><Input type="number" onChange={onUpdateAssessment('msUpperAbs')} value={assessment.msUpperAbs} unit="cm" /></div>
 
-      <div className="justify-self-end">Lower abs</div>
+      <div>Lower abs</div>
       <div><Input type="number" onChange={onUpdateAssessment('msLowerAbs')} value={assessment.msLowerAbs} unit="cm" /></div>
 
-      <div className="justify-self-end">Hips</div>
+      <div>Hips</div>
       <div><Input type="number" onChange={onUpdateAssessment('msLowerAbs')} value={assessment.msLowerAbs} unit="cm" /></div>
 
       <div className="col-span-2 mt-4 justify-self-end">
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,10 @@
 function Input({ type, value, onChange, unit }) {
-  const marginForLabel = unit === undefined ? "0" : "6";
+  const paddingForLabel = unit === undefined ? "" : "pr-8";
+  const marginForLabel =  unit === undefined ? "" : "-ml-6";
   return (
-    <div className="inline-block w-full border-solid border-2 border-cyan-500 has-[:focus]:border-cyan-400 rounded-lg px-2 py-1">
-      <input className={`focus:outline-none w-full min-w-10 pr-${marginForLabel}`} type={type} onChange={onChange} value={value} />
-      <span className={`-ml-${marginForLabel}`}>{unit}</span>
+    <div className="whitespace-nowrap inline-block w-full min-w-16 border-solid border-2 border-cyan-500 has-[:focus]:border-cyan-400 rounded-lg px-2 py-1">
+      <input className={`focus:outline-none w-full ${paddingForLabel}`} type={type} onChange={onChange} value={value} />
+      <span className={marginForLabel}>{unit}</span>
     </div>
   );
 }
